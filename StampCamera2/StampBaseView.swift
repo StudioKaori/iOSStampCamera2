400c5c6069070cc454e490d037289a152941210d
Studio Kaori <studiokaori.com@gmail.com>
2022-04-06 11:50:42 +0200

    Add: stampbaseView

--- StampCamera2/StampBaseView.swift
+++ StampCamera2/StampBaseView.swift
@@ -18,6 +18,26 @@ class StampBaseView: UIView {
     func addStamp(stampImage: UIImage){
         let size = 100
         let stampView = StampView()
+        stampView.image = stampImage
+        stampView.frame = CGRect(x: 0, y: 0, width: size, height: size)
+        stampView.center = self.center
+        stampView.isUserInteractionEnabled = true
+        self.addSubview(stampView)
+    }
+    
+    func deleteStamp() {
+        if let topStamp = self.subviews.last as? StampView {
+            topStamp.removeFromSuperview()
+        }
+    }
+    
+    func saveImageWithStamps(){
+        UIGraphicsBeginImageContextWithOptions(self.frame.size, self.isOpaque, 0.0)
+        guard let context = UIGraphicsGetCurrentContext() else {return}
+        self.layer.render(in: context)
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else {return}
+        UIGraphicsEndImageContext()
+        UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
     }
 
 }
